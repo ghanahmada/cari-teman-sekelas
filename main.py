552d731227c52fbacffae19671792a830bf9b5e3
Ghana Ahmada Yudistira <103763579+ghanahmada@users.noreply.github.com>
2022-08-22 09:29:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,33 +9,41 @@ JUDUL = st.title("Cari Teman Sekelas")
 data = pd.read_csv("Pembagian-Kelas-Mahasiswa-Angkatan-2022.csv")
 data = data.drop(["NPM"],axis=1).set_index("No").fillna("-")
 
-# Membuat list nama mahasiswa dan menampilkan data mahasiswa tersebut
+# Membuat data user
 nama_list = sorted(tuple(data["Nama Mahasiswa"].values.tolist()))
 user_name = st.selectbox("Masukkan Nama Lengkap Kamu", nama_list)
 user_data = data[data["Nama Mahasiswa"] == user_name].reset_index(drop=True)
-user_data.index += 1
-st.table(user_data)
+
+# Mengubah data user ke list
+user_list = user_data.values.tolist()
+
+# Merapihkan data user dan menampilkannya
+user_data2 = user_data.drop(["PSD"],axis=1) if user_list[0][1][:2] == "SI" else user_data.drop(["Manbis","Kombistek"],axis=1)
+user_data2.index += 1
+st.table(user_data2)
 
 # Membuat kolom kanan dan kiri
 left, right = st.columns(2)
 
 # Meminta list mata kuliah yang diinginkan
-matkul = ["DDP 1","Matdis 1","Kalkulus 1","PSD","Manbis","Kombistek"]
+matkul_SI = ["DDP 1","Matdis 1","Kalkulus 1","Manbis","Kombistek"]
+matkul_IK = ["DDP 1","Matdis 1","Kalkulus 1","PSD"]
+matkul = matkul_SI if user_list[0][1][:2] == "SI" else matkul_IK
 mata_kuliah = right.multiselect("Pilih Mata Kuliah", matkul)
 
 # Meminta nama mahasiswa yang ingin dibandingkan
 target_name = left.selectbox("Pilih mahasiswa yang ingin dibandingkan", nama_list)
 target_data = data[data["Nama Mahasiswa"] == target_name]
 
+# Mengubah data target ke dictionary
+target_dict = target_data[matkul_IK + ["Manbis","Kombistek"]].to_dict("records")[0]
+
+# Menggabungkan data user dengan mahasiswa yang dibandingkan
 compare_data = pd.concat([user_data, target_data]).reset_index(drop=True)
 compare_data.index += 1
 
 # Nilai awal
 custom_data, check = "", 0
-
-# Mengubah data user ke list dan data target ke dictionary
-user_list = user_data.values.tolist()
-target_dict = target_data[matkul].to_dict("records")[0]
 
 # Menghitung kelas mata kuliah yang sama antara user dengan target
 matkul_sama = [b for a,b in zip(user_list[0][2:], target_dict) if a != "-" and a == target_dict[b]]
@@ -44,7 +52,7 @@ matkul_sama2 = ", ".join(matkul_sama)
 
 # Membuat fungsi untuk menampilkan data mahasiswa yang memiliki persamaan tipe kelas dengan user
 def similarity(pelajaran, df):
-    num = matkul.index(pelajaran) + 2
+    num = (matkul_IK + ["Manbis","Kombistek"]).index(pelajaran) + 2
     return df[df[pelajaran] == user_list[0][num]]
 
 # Merapihkan data sesuai dengan fungsi "similarity"
@@ -56,15 +64,26 @@ for value in mata_kuliah:
     else:
         custom_data = similarity(value, custom_data)
 
+# Memanggil nama target
+def filter_name(name):
+    res, a = name.split(), ["Muh","Moh","Moch"]
+    for value in a:
+        if res[0].startswith(value):
+            return res[1]
+    return res[0]
+
 # tampilan kiri
 if target_name == user_name:
     pass
 elif len(matkul_sama) > 0:
-    left.markdown(f"Kamu dan {target_name.split()[0]} memiliki **{len(matkul_sama)} kelas matkul** yang sama, yaitu pada matkul:")
+    if len(matkul_sama) == 1:
+        left.markdown(f"Kamu dan {filter_name(target_name)} hanya memiliki **{len(matkul_sama)} kelas matkul** yang sama, yaitu pada matkul:")
+    else:
+        left.markdown(f"Kamu dan {filter_name(target_name)} hanya memiliki **{len(matkul_sama)} kelas matkul** yang sama, yaitu pada matkul:")
     left.markdown(f"\t**{matkul_sama2}**")
     left.table(compare_data)
 elif len(matkul_sama) == 0:
-    left.markdown(f"Kamu dan {target_name.split()[0]} tidak memiliki kelas matkul yang sama :(")
+    left.markdown(f"Kamu dan {filter_name(target_name)} tidak memiliki kelas matkul yang sama :(")
 
 # tampilan kanan
 if mata_kuliah != []:
